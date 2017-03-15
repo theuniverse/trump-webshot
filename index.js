cb464eb0c5995648f2a766011a5650434d0b51a9
Qi Xi <xiqish@cn.ibm.com>
2017-03-15 22:11:06 +0800

    new config file and some change on the title and content

--- index.js
+++ index.js
@@ -35,6 +35,19 @@ async function capture(selector, name) {
   });
 }
 
+async function exec_sync(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.log(error);
+        reject();
+      }
+
+      resolve();
+    });
+  });
+}
+
 (async function() {
   console.log('Start retrieving ' + URL_TO_VISIT);
   const instance = await phantom.create();
@@ -56,7 +69,7 @@ async function capture(selector, name) {
     const day = chart.childNodes[0].getElementsByClassName('mouse-guide')[0].childNodes[0].textContent;
 
     return {
-      title: [day, ': ', approved, ' (👍) - ', disapproved, ' (👎)'].join(''),
+      title: [day, ' - ', approved, '% 👍 : ', disapproved, '% 👎'].join(''),
       day: day.replace(' ', '-').toLowerCase()
     };
   });
@@ -75,17 +88,11 @@ async function capture(selector, name) {
   await capture('div.chart.main', trendPic);
   await capture('div.polls', pollsPic);
 
-  exec('cd blog && hexo new "' + postTitle + '"', (error, stdout, stderr) => {
-    if(error) {
-      console.log(error);
-      return;
-    }
+  const TREND_POLLS_LINE = '![Trend](/images/' + trendPic + ')\n\n![Polls](/images/' + pollsPic + ')\n';
+  const FOOT_NOTE = 'More to visit ***[How (un)popular is Donald Trump?](https://projects.fivethirtyeight.com/trump-approval-ratings/)*** on FiveThirtyEight';
+  const ALL_CONTENT = TREND_POLLS_LINE + FOOT_NOTE;
 
-    exec('mv *.png blog/source/images/ && echo "![Trend](/images/' + trendPic + ')\n\n![Polls](/images/' + pollsPic + ')" >> "blog/source/_posts/' + postUrl + '.md"', (error, stdout, stderr) => {
-      if(error) {
-        console.log(error);
-        return;
-      }
-    });
-  });
+  await exec_sync('cd blog && hexo new "' + postTitle + '"');
+  await exec_sync('mv *.png blog/source/images/');
+  await exec_sync('echo "' + ALL_CONTENT + '" >> "blog/source/_posts/' + postUrl + '.md"');
 }());
