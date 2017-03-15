386ca5b31a871eb6c0bf6c6b76d2456bc0607b1f
Qi Xi <xiqish@cn.ibm.com>
2017-03-15 17:35:33 +0800

    make it work

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const webshot = require('webshot');
 const phantom = require('phantom');
+const exec = require('child_process').exec;
 
 const URL_TO_VISIT = 'https://projects.fivethirtyeight.com/trump-approval-ratings/';
 
@@ -26,22 +27,47 @@ function capture(selector, name) {
   );
 }
 
-capture('div.chart.main', 'trend.png');
-capture('div.polls', 'polls.png');
-
 (async function() {
   const instance = await phantom.create();
   const page = await instance.createPage();
   const status = await page.open(URL_TO_VISIT);
   console.log(status);
 
-  const innerHTML = await page.evaluate(function() {
+  const postData = await page.evaluate(function() {
     const chart = document.querySelector('div.chart');
     const approved = chart.childNodes[0].getElementsByClassName('label approve fg')[0].childNodes[0].textContent;
     const disapproved = chart.childNodes[0].getElementsByClassName('label disapprove fg')[0].childNodes[0].textContent;
     const day = chart.childNodes[0].getElementsByClassName('mouse-guide')[0].childNodes[0].textContent;
-    return [day, ': ', approved, ' (Approved) - ', disapproved, ' (Disapproved).'].join('');
+
+    return {
+      title: [day, ': ', approved, ' (👍) - ', disapproved, ' (👎)'].join(''),
+      day: day.replace(' ', '-').toLowerCase()
+    };
   });
 
+  console.log(postData);
+
   await instance.exit();
+
+  // const postTitle = 'Day 45: 44.5% (👍) - 45.5% (👎)';
+  const postTitle = postData.title;
+  const postDay = postData.day;
+  const postUrl = postTitle.replace(/[^a-zA-Z0-9\uD83D\uDC4D\uD83D\uDC4E]+/g, '-').replace(/-$/g, '');
+
+  capture('div.chart.main', 'trend-' + postDay + '.png');
+  capture('div.polls', 'polls' + postDay + '.png');
+
+  exec('cd blog && hexo new "' + postTitle + '"', (error, stdout, stderr) => {
+    if(error) {
+      console.log(error);
+      return;
+    }
+
+    exec('cp *.png blog/source/images/ && echo "![Trend](/images/trend.png)\n\n![Polls](/images/polls.png)" >> "blog/source/_posts/' + postUrl + '.md"', (error, stdout, stderr) => {
+      if(error) {
+        console.log(error);
+        return;
+      }
+    });
+  });
 }());
