72d3814feba87e82348371d82e0cef1f2156cfe4
Qi Xi <xiqish@cn.ibm.com>
2018-04-18 16:07:40 +0800

    Use IFTTJ and upgrade Node version

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-const webshot = require('webshot');
 const phantom = require('phantom');
 const exec = require('child_process').exec;
 
 const URL_TO_VISIT = 'https://projects.fivethirtyeight.com/trump-approval-ratings/';
+const URL_TO_POST = 'https://ifttj.wong2.me/Sk09tE7hM';
 
-async function sleep(timeout) {
-  return new Promise((resolve, reject) => {
-    setTimeout(function() {
-      resolve();
-    }, timeout);
-  });
-}
-
-async function capture(selector, name) {
-  return new Promise((resolve, reject) => {
-    webshot(
-      URL_TO_VISIT,
-      name,
-      {
-        captureSelector: selector,
-        width: 1024,
-        height: 550
-      },
-      function(err) {
-        if (err) {
-          console.log(err);
-          reject();
-        } else {
-          console.log(name + ' captured');
-          resolve();
-        }
-      }
-    );
-  });
-}
-
-async function exec_sync(command) {
+const exec_sync = async (command) => {
   return new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
       if (error) {
@@ -46,10 +15,10 @@ async function exec_sync(command) {
       resolve();
     });
   });
-}
+};
 
-(async function() {
-  console.log('Start retrieving ' + URL_TO_VISIT);
+(async () => {
+  console.log(`Start retrieving ${URL_TO_VISIT}`);
   const instance = await phantom.create();
   const page = await instance.createPage();
   const status = await page.open(URL_TO_VISIT, {
@@ -58,7 +27,7 @@ async function exec_sync(command) {
   });
   console.log('Status: ' + status);
 
-  const postData = await page.evaluate(function() {
+  const postData = await page.evaluate(function () {
     const chart = document.querySelector('div.chart');
     const approved = chart.childNodes[0].getElementsByClassName('label approve fg')[0].childNodes[0].textContent;
     const disapproved = chart.childNodes[0].getElementsByClassName('label disapprove fg')[0].childNodes[0].textContent;
@@ -74,20 +43,10 @@ async function exec_sync(command) {
   await instance.exit();
 
   const postTitle = postData.title;
-  const postDay = postData.day;
-  const postUrl = postTitle.replace(/[^a-zA-Z0-9\uD83D\uDC4D\uD83D\uDC4E]+/g, '-').replace(/-$/g, '');
+  const postFootNote = '点进来了解更多';
 
-  const trendPic = 'trend-' + postDay + '.png';
-  const pollsPic = 'polls-' + postDay + '.png';
+  const ALL_CONTENT = `${postTitle}\n${postFootNote}\n===\n${URL_TO_VISIT}`;
+  console.log(ALL_CONTENT);
 
-  await capture('div.chart.main', trendPic);
-  await capture('div.polls', pollsPic);
-
-  const TREND_POLLS_LINE = '![Trend](/images/' + trendPic + ')\n\n![Polls](/images/' + pollsPic + ')\n\n';
-  const FOOT_NOTE = 'More to visit ***[How (un)popular is Donald Trump?](https://projects.fivethirtyeight.com/trump-approval-ratings/)*** on FiveThirtyEight';
-  const ALL_CONTENT = TREND_POLLS_LINE + FOOT_NOTE;
-
-  await exec_sync('cd blog && hexo new "' + postTitle + '"');
-  await exec_sync('mv *.png blog/source/images/');
-  await exec_sync('echo "' + ALL_CONTENT + '" >> "blog/source/_posts/' + postUrl + '.md"');
-}());
+  await exec_sync(`curl -d "${ALL_CONTENT}" -H "Content-Type:text/plain" -X POST ${URL_TO_POST}`);
+})();
